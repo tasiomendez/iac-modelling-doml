990b1e2d8b300bfe292ffb56aba32ca3d44376f0
Tasio Mendez <tm.tasio@gmail.com>
2021-05-05 17:42:47 +0200

    Scoping data_types

--- org.piacere.dsl.rmdf/src/org/piacere/dsl/RMDFRuntimeModule.java
+++ org.piacere.dsl.rmdf/src/org/piacere/dsl/RMDFRuntimeModule.java
@@ -3,9 +3,16 @@
  */
 package org.piacere.dsl;
 
+import org.eclipse.xtext.scoping.IGlobalScopeProvider;
+import org.eclipse.xtext.scoping.impl.ImportUriGlobalScopeProvider;
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class RMDFRuntimeModule extends AbstractRMDFRuntimeModule {
+	
+	public Class<? extends IGlobalScopeProvider> bindIGlobalScopeProvider() {
+		return ImportUriGlobalScopeProvider.class;
+	}
+	
 }
