1205529f75b88e0aa6992dd0100bb68702b6df0c
Tasio Mendez <tm.tasio@gmail.com>
2021-05-11 18:11:43 +0200

    Variable scopes & imports

--- org.piacere.dsl.rmdf/src/org/piacere/dsl/RMDFRuntimeModule.java
+++ org.piacere.dsl.rmdf/src/org/piacere/dsl/RMDFRuntimeModule.java
@@ -4,15 +4,26 @@
 package org.piacere.dsl;
 
 import org.eclipse.xtext.scoping.IGlobalScopeProvider;
+import org.eclipse.xtext.scoping.IScopeProvider;
+import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
 import org.eclipse.xtext.scoping.impl.ImportUriGlobalScopeProvider;
+import org.eclipse.xtext.scoping.impl.SimpleLocalScopeProvider;
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class RMDFRuntimeModule extends AbstractRMDFRuntimeModule {
 	
+	@Override
 	public Class<? extends IGlobalScopeProvider> bindIGlobalScopeProvider() {
 		return ImportUriGlobalScopeProvider.class;
+	}
+	
+	public void configureIScopeProviderDelegate(Binder binder) {
+		binder.bind(IScopeProvider.class).annotatedWith(Names.named(AbstractDeclarativeScopeProvider.NAMED_DELEGATE))
+			.to(SimpleLocalScopeProvider.class);
 	}
 	
 }
--- org.piacere.dsl.rmdf/src/org/piacere/dsl/scoping/RMDFScopeProvider.java
+++ org.piacere.dsl.rmdf/src/org/piacere/dsl/scoping/RMDFScopeProvider.java
@@ -3,13 +3,11 @@
  */
 package org.piacere.dsl.scoping;
 
-
 /**
  * This class contains custom scoping description.
  * 
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#scoping
  * on how and when to use it.
  */
-public class RMDFScopeProvider extends AbstractRMDFScopeProvider {
-
+public class RMDFScopeProvider extends AbstractRMDFScopeProvider {	
 }
