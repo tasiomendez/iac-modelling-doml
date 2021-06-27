3077aa7d8655d1f43d2ceed737c84b145d135948
Tasio Mendez <tm.tasio@gmail.com>
2021-06-27 17:33:35 +0200

    Scoping inheritance

--- org.piacere.dsl.rmdf/src/org/piacere/dsl/scoping/RMDFScopeProvider.java
+++ org.piacere.dsl.rmdf/src/org/piacere/dsl/scoping/RMDFScopeProvider.java
@@ -5,12 +5,16 @@ package org.piacere.dsl.scoping;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IResourceDescription;
+import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.Scopes;
 import org.eclipse.xtext.scoping.impl.FilteringScope;
@@ -32,18 +36,44 @@ import com.google.inject.Inject;
 public class RMDFScopeProvider extends AbstractRMDFScopeProvider {	
 
 	@Inject
+	private IResourceDescriptions descriptions;
+
+	@Inject
 	IResourceDescription.Manager mgr;
-		
+
 	@Override
 	public IScope getScope(EObject context, EReference reference) {
-		
+
 		if (reference == RMDFPackage.Literals.CNODE_PROPERTY__NAME) {
 			Map<CProperty, QualifiedName> properties = Helper.getAllCProperty(context, true);
+
+			EObject container = Helper.getContainer(context);
+			Iterable<IEObjectDescription> elements = descriptions.getExportedObjectsByType(RMDFPackage.Literals.CNODE_TYPE);
+
+			// This block adds to the Scope the properties of the resources that 
+			// extends the given one, so properties can be overwritten.
+			List<CNodeType> extendables = StreamSupport.stream(elements.spliterator(), false)
+					.map((node) -> (CNodeType) EcoreUtil2.resolve(node.getEObjectOrProxy(), context))
+					.filter((node) -> {
+						if (container instanceof CNodeType &&
+								node != null &&
+								node.getData() != null &&
+								node.getData().getSuperType() != null) {
+							return ((CNodeType) container).getName().equals(node.getData().getSuperType().getName());
+						} 
+						return false;
+					})
+					.collect(Collectors.toList());
+			extendables.forEach((node) -> {
+				properties.putAll(Helper.getCPropertyFromNodeType(node, null));
+				properties.putAll(Helper.getCPropertyFromNodeTypeNodeTemplates(node, null));
+			});
+
 			return Scopes.scopeFor(properties.keySet(), (s) -> {
 				return properties.get(s);
 			}, IScope.NULLSCOPE);
 		}
-		
+
 		if (reference == RMDFPackage.Literals.CNODE__TYPE ||
 				reference == RMDFPackage.Literals.CNODE_TYPE_DATA__SUPER_TYPE) {
 			return new FilteringScope(this.getImportedScope(context, reference), (s) -> {
@@ -51,7 +81,7 @@ public class RMDFScopeProvider extends AbstractRMDFScopeProvider {
 				return (obj instanceof CNodeType);
 			});
 		}
-		
+
 		if (reference == RMDFPackage.Literals.CNODE_CROSS_REF_GET_VALUE__CROSSVALUE) {
 			if (((CNodeCrossRefGetValue) context).isSuper()) {
 				CNodeType container = EcoreUtil2.getContainerOfType(context, CNodeType.class);
@@ -61,14 +91,14 @@ public class RMDFScopeProvider extends AbstractRMDFScopeProvider {
 				}, IScope.NULLSCOPE);
 			}
 		}
-		
+
 		EObject root = EcoreUtil2.getRootContainer(context);
 		return new FilteringScope(super.getScope(context, reference), (s) -> {
 			EObject inneroot = EcoreUtil2.getRootContainer(s.getEObjectOrProxy());
 			return root.equals(inneroot);
 		});
 	}
-		
+
 	/**
 	 * Returns a scope of the given context only for imported namespace
 	 * 
@@ -84,7 +114,7 @@ public class RMDFScopeProvider extends AbstractRMDFScopeProvider {
 			EObject inneroot = EcoreUtil2.getRootContainer(s.getEObjectOrProxy());
 			if (root.equals(inneroot))
 				return true;
-			
+
 			return imports.stream().anyMatch((i) -> {		
 				if (i.getImportedName() == null)
 					return false;
@@ -98,5 +128,5 @@ public class RMDFScopeProvider extends AbstractRMDFScopeProvider {
 			});
 		});
 	}
-		
+
 }
